626180aaaf748a331e57cb4024df610314877dab
Kang Noir <93761595+noircode@users.noreply.github.com>
2023-01-10 05:08:56 +0000

    my first commit

---
+++ src/components/common/Button.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Button from '@mui/material/Button';
+
+function BasicButtons() {
+  return (
+    <Button variant="contained">Log in</Button>
+  );
+}
+
+export default BasicButtons;
